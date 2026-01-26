3c833775eb3c0b9fc1e7eb5f9b41f8652c3e2a37
zuu-maker <zulumkhuzo@gmail.com>
2026-01-26 21:18:33 +0200

    updated resume link #3

--- components/config.js
+++ components/config.js
@@ -6,7 +6,7 @@ export const CONFIG = {
     "I transform complex problems into elegant solutions. Full-stack developer with expertise in AI, machine learning, and modern web technologies.",
   github: "https://github.com/zuu-maker",
   resumeUrl:
-    "https://firebasestorage.googleapis.com/v0/b/resume-903bf.appspot.com/o/pdf%2Fmkhuzo%20j%20zulu%20cv.pdf?alt=media&token=3b615c9b-4463-4c1d-a565-977d292f54bf",
+    "https://firebasestorage.googleapis.com/v0/b/gifted-minds-62bb0.firebasestorage.app/o/resume%2Fcv_mkhuzo_j_zulu.pdf?alt=media&token=bfd67996-0bc9-4266-b25a-1d1b7917dfa9",
   email: "mkhuzozulu@outlook.com",
 
   // EmailJS Config
